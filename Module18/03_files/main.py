038c9cf713404ab4a94a079ec0f31f787f31999e
Alexander Olhovik <aao.t@yandex.ru>
2021-07-25 14:14:37 +0300

    урок 1, недочёты в 2 и 3

--- Module18/03_files/main.py
+++ Module18/03_files/main.py
@@ -3,7 +3,8 @@ forbidden_symbols = '@№$%^&*()'
 
 start_with = [file_name.startswith(symbol) for symbol in forbidden_symbols]
 
-if max(start_with):
+if max(start_with):  # TODO Достаточно мудрёный способ, просто проверьте вхождение первой буквы в строку запрещённых
+                     #  символов с помощью оператора in
     print('Ошибка: название начинается на один из специальных символов')
 elif not (file_name.endswith('.txt') or file_name.endswith('.docx')):
     print('Ошибка: неверное расширение файла. Ожидалось .txt или .docx')
