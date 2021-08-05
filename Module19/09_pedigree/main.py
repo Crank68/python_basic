1cce5f64b7314e4995e556cb8608a468d565b0cd
Crank201@gmail.com <Ilwaikua7388422>
2021-08-05 17:31:43 +0300

    Задание 9

--- Module19/09_pedigree/main.py
+++ Module19/09_pedigree/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+family = dict()
+people_count = int(input('Введите количество человек: '))
+
+for pair_number in range(1, people_count):
+	couple = input(f'{pair_number} пара: ').split()
+	if family.get(couple[1]) is None:
+		family[couple[1]] = 0
+		family[couple[0]] = 1
+	else:
+		family[couple[0]] = family[couple[1]] + 1
+
+print('\n“Высота” каждого члена семьи:')
+for key in sorted(family.keys()):
+	print(key, family[key])
