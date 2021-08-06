9b6b184e4575d86a919ec2bf03ee430cfe3289e0
danila_troshin <Crank201@gmail.com>
2021-08-06 06:09:28 +0300

    10 задание

--- Module19/10_palindrome_again/main.py
+++ Module19/10_palindrome_again/main.py
@@ -1,1 +1,7 @@
-# TODO здесь писать код
+text = input('Введите строку: ')
+set_text = set(text)
+list_symbols_count = [0 for symbol in set_text if text.count(symbol) % 2 != 0]
+if 0 <= len(list_symbols_count) <= 1:
+    print('Можно сделать палиндромом')
+else:
+    print('Нельзя сделать палиндромом')
